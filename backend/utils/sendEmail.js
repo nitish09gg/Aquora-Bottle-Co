b7bbe1c06b140b8b115b0fb1ccc673fc575d381b
Sanjit Singh <sanjit420sanju@gmail.com>
2026-08-07 01:00:38 +0530

    Added Reset Password Function 9

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -9,20 +9,26 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendEmail = async ({ to, subject, html }) => {
-  console.log("EMAIL_USER:", process.env.EMAIL_USER);
-  console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
+  try {
+    console.log("EMAIL_USER:", process.env.EMAIL_USER);
+    console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
 
-  await transporter.verify();
-  console.log("SMTP Connection Successful");
+    await transporter.verify();
+    console.log("SMTP Connection Successful");
 
-  await transporter.sendMail({
-    from: `"Aquora Bottle Co." <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+    await transporter.sendMail({
+      from: `"Aquora Bottle Co." <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
 
-  console.log("Mail Sent Successfully");
+    console.log("Mail Sent Successfully");
+  } catch (err) {
+    console.error("Nodemailer Error:");
+    console.error(err);
+    throw err;
+  }
 };
 
 module.exports = sendEmail;

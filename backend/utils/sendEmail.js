b82ce0f47bb4e513e4fc7439b6d349070794c9f1
Sanjit Singh <sanjit420sanju@gmail.com>
2026-08-07 13:46:13 +0530

    Forgot Password Testing 4

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,20 +3,21 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: true,
+  secure: false,          // true only for port 465
+  requireTLS: true,       // use this with port 587
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-
   connectionTimeout: 10000,
   greetingTimeout: 10000,
   socketTimeout: 10000,
 });
-const sendEmail = async ({ to, subject, html }) => {
-  console.log("Connecting to SMTP...");
 
+const sendEmail = async ({ to, subject, html }) => {
   try {
+    console.log("Connecting to SMTP...");
+
     await transporter.verify();
     console.log("SMTP verified!");
 

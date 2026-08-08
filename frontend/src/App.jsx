98c92b330b2ccfb3add95d9cf03691be42afbb86
Sanjit Singh <sanjit420sanju@gmail.com>
2026-08-08 14:41:53 +0530

    Email Otp Verification 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -7,78 +8,82 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import PublicRoute from "./components/PublicRoute";
 import ResetPassword from "./pages/ResetPassword";
 import ForgotPassword from "./pages/ForgotPassword";
+
 import { useAuth } from "./context/AuthContext";
 import LoadingScreen from "./components/LoadingScreen";
 
 function App() {
-
   const { pageLoading } = useAuth();
 
-  if (pageLoading) {
-    return <LoadingScreen />;
-  }
   return (
-    <Routes>
-      {/* Home */}
-      <Route
-        path="/"
-        element={
-          <PublicRoute>
+    <>
+      <Routes>
+        {/* Home */}
+        <Route
+          path="/"
+          element={
             <Home />
-          </PublicRoute>
-        }
-      />
+          }
+        />
 
-      {/* Login */}
-      <Route
-        path="/login"
-        element={
-          <PublicRoute>
-            <Login />
-          </PublicRoute>
-        }
-      />
+        {/* Login */}
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
 
-      {/* Signup */}
-      <Route
-        path="/signup"
-        element={
-          <PublicRoute>
-            <Signup />
-          </PublicRoute>
-        }
-      />
+        {/* Signup */}
+        <Route
+          path="/signup"
+          element={
+            <PublicRoute>
+              <Signup />
+            </PublicRoute>
+          }
+        />
 
-      {/* Forgot Password */}
-      <Route
-        path="/forgot-password"
-        element={
-          <PublicRoute>
-            <ForgotPassword />
-          </PublicRoute>
-        }
-      />
+        {/* Forgot Password */}
+        <Route
+          path="/forgot-password"
+          element={
+            <PublicRoute>
+              <ForgotPassword />
+            </PublicRoute>
+          }
+        />
 
-      {/* Reset Password */}
-      <Route
-        path="/reset-password/:token"
-        element={
-          <PublicRoute>
-            <ResetPassword />
-          </PublicRoute>
-        }
-      />
+        {/* Reset Password */}
+        <Route
+          path="/reset-password/:token"
+          element={
+            <PublicRoute>
+              <ResetPassword />
+            </PublicRoute>
+          }
+        />
 
-      {/* Protected Browse */}
-      <Route
-        path="/browse"
-        element={
-          <ProtectedRoute>
-            <Browse />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
+        {/* Protected Browse */}
+        <Route
+          path="/browse"
+          element={
+            <ProtectedRoute>
+              <Browse />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+
+      {/* Global action loading screen */}
+      {pageLoading && (
+        <div className="fixed inset-0 z-[9999]">
+          <LoadingScreen />
+        </div>
+      )}
+    </>
   );
 }
 

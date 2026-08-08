57d03cb4935aa5e9c1636547eca76bfb0853efff
Sanjit Singh <sanjit420sanju@gmail.com>
2026-08-08 15:10:29 +0530

    Debugging Email Otp Verification

--- backend/schemas/UserSchema.js
+++ backend/schemas/UserSchema.js
@@ -58,18 +58,16 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   if (!this.isModified("password")) {
-    return next();
+    return;
   }
 
   if (this.$locals.passwordAlreadyHashed) {
-    return next();
+    return;
   }
 
   this.password = await bcrypt.hash(this.password, 10);
-
-  next();
 });
 
 userSchema.methods.comparePassword = function (enteredPassword) {
